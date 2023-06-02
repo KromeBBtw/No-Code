3145ce168bfc169b9cbf554294cfcc321156cbe9
Krome <122440060+KromeBBtw@users.noreply.github.com>
2023-06-02 07:35:41 +0000

    added content to main.cpp
    created all necessary code to run main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-
 #include "developer.hpp" 
 
+auto main() -> int
+{
+    std::vector<std::shared_ptr<developer>> developers;
+
+    std::shared_ptr<JuniorDeveloper> junior_developer = std::make_shared<JuniorDeveloper>("Malian", "Krome");
+    std::shared_ptr<JuniorDeveloper> senior_developer = std::make_shared<JuniorDeveloper>("Linus", "SunilRednael");
+
+    developers.push_back(junior_developer);
+    developers.push_back(senior_developer);
+
+    for (const auto& itr : developers)
+    {
+        itr->solve_problem();
+        std::cout << std::endl;
+    }
+
+    return 0;
+
+}
