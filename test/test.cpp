818f04c7d69b2309d13c2ca53b5a357bf2ede468
SunilRednael <122459030+SunilRednael@users.noreply.github.com>
2023-06-12 09:38:50 +0000

    first Google Test
    first try to implement a google test and to run it.

---
+++ test/test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include "developer.hpp"
+
+// Test fixture for Developer tests
+class DeveloperTest : public ::testing::Test {
+    protected:
+    // Test objects
+    SeniorDeveloper seniorDev{"Alice", "The Ace"};
+    JuniorDeveloper juniorDev{"Bob", "The Rookie"};
+};
+
+//Test the constructors of SeniorDeveloper and JuniorDeveloper
+TEST_F(DeveloperTest, ConstructorTets) {
+    //Check that the name and alias are initalized correctly for SeniorDeveloper
+    EXPECT_EQ(seniorDev.get_dev_name(), "Alice");
+    EXPECT_EQ(seniorDev.get_dev_alias(), "The Ace");
+
+    // Check that the name and alias are initalized correctly for JuniorDeveloper
+    EXPECT_EQ(juniorDev.get_dev_name(), "Bob");
+    EXPECT_EQ(juniorDev.get_dev_alias(); "The Rookie");
+}
