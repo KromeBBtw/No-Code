2de72d6d4bf4bfc89b331e6bd63edb27bb81c89e
SunilRednael <122459030+SunilRednael@users.noreply.github.com>
2023-06-15 07:38:38 +0000

    little changes in test file

--- test/test.cpp
+++ test/test.cpp
@@ -5,17 +5,17 @@
 class DeveloperTest : public ::testing::Test {
     protected:
     // Test objects
-    SeniorDeveloper seniorDev{"Alice", "The Ace"};
-    JuniorDeveloper juniorDev{"Bob", "The Rookie"};
+    SeniorDeveloper seniorDev{"Malian", "Krome"};
+    JuniorDeveloper juniorDev{"Linus", "Sunil Rednael"};
 };
 
 //Test the constructors of SeniorDeveloper and JuniorDeveloper
 TEST_F(DeveloperTest, ConstructorTets) {
     //Check that the name and alias are initalized correctly for SeniorDeveloper
-    EXPECT_EQ(seniorDev.get_name(), "Alice");
-    EXPECT_EQ(seniorDev.get_alias(), "The Ace");
+    EXPECT_EQ(seniorDev.get_name(), "Malian");
+    EXPECT_EQ(seniorDev.get_alias(), "Krome");
 
     // Check that the name and alias are initalized correctly for JuniorDeveloper
-    EXPECT_EQ(juniorDev.get_name(), "Bob");
-    EXPECT_EQ(juniorDev.get_alias(), "The Rookie");
+    EXPECT_EQ(juniorDev.get_name(), "Linus");
+    EXPECT_EQ(juniorDev.get_alias(), "Sunil Rednael");
 }
